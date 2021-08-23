bb4cda0e3d665c457b78ed866f6ea3b135cdf1a5
BlackSquirrelz <tobias.weisskopf.ch@gmail.com>
2021-08-23 11:11:27 +0200

    Updates Main File and Readme
    Mainfile added copy that preserves timestamps
    
    Readme added installation instructions and references

--- get_system_info.sh
+++ get_system_info.sh
@@ -7,34 +7,35 @@
 # TODO! Ask user to enter a case number, a analyst etc.
 #read "Please enter a case number"
 filename=0000_test_file.txt
+locations=locations.conf
 
 # Check if Folders already exist
-if ! -d SystemInformation
-then
-    mkdir SystemInformation
-else
-    echo "Warning "
-fi
+directories=("SystemInformation" "Logs" "InstalledApplications")
 
-if ! -d InstalledApplications
-then
-    mkdir InstalledApplications
-fi
+for DIRECTORY in "${directories[@]}"
+do
+    if [ -d "$DIRECTORY"]; then
+        rm -rf $DIRECTORY
+    else
+        mkdir $DIRECTORY
+    fi
+done
 
-if ! -d Logs
-then
-    mkdir Logs
-fi
+# FEATURE add data to configuration file.
+# Loading Data from the configuration file
+while read -r plist_path; do 
+    get_plist_artifact $plist_path
+done < $locations
 
 # Function to get all PLIST Artifacts
 get_plist_artifact() {
     plutil -p $plist_path >> $filename
-    cp $plist_path $artifact_category
+    cp -p $plist_path $artifact_category
 }
 
 # Copying Logfiles
 get_logfiles(){
-    cp $logfile_path Logs/
+    cp -p $logfile_path Logs/
 }
 
 
@@ -46,10 +47,10 @@ echo "Getting System Information"
 
 # System Information
 plutil -p /System/Library/CoreServices/SystemVersion.plist
-cp /System/Library/CoreServices/SystemVersion.plist SystemInformation/
+cp -p /System/Library/CoreServices/SystemVersion.plist SystemInformation/
 
 # Time of installation
-cp '/private/var/db/.Apple*' SystemInformation/
+cp -p '/private/var/db/.Apple*' SystemInformation/
 
 # Get TimeZone information
 plutil -p /Library/Preferences/.GlobalPreferences.plist
@@ -65,7 +66,7 @@ echo "Installed Applications"
 
 # Installation History
 plutil -p /Library/Receipts/InstallHistory.plist
-cp /Library/Receipts/InstallHistory.plist InstalledApplications/
+cp -p /Library/Receipts/InstallHistory.plist InstalledApplications/
 
 # Printing File System Entries from /var/db/receipts
 ls /var/db/receipts
@@ -74,26 +75,26 @@ ls /var/db/receipts
 
 # Software Update List
 plutil -p /Library/Preferences/com.apple.SoftwareUpdate.plist
-cp /Library/Preferences/com.apple.SoftwareUpdate.plist InstalledApplications/
+cp -p /Library/Preferences/com.apple.SoftwareUpdate.plist InstalledApplications/
 
 # User specific Updatejournal
 plutil -p '~/Library/Application Support/App Store/updatejournal.plist'
-cp '~/Library/Application Support/App Store/updatejournal.plist' InstalledApplications/
+cp -p '~/Library/Application Support/App Store/updatejournal.plist' InstalledApplications/
 
 # Global AppStore Settings
 plutil -p /Library/Preferences/com.apple.commerce.plist
-cp /Library/Preferences/com.apple.commerce.plist InstalledApplications/
+cp -p /Library/Preferences/com.apple.commerce.plist InstalledApplications/
 
 # User Specific AppStore Settings
 plutil -p '~/Library/Preferences/com.apple.commerce.plist'
-cp '~/Library/Preferences/com.apple.commerce.plist' InstalledApplications/
+cp -p '~/Library/Preferences/com.apple.commerce.plist' InstalledApplications/
 
 # --------------- LOG FILES -------------------------
 
 echo "Copying Log Files"
 
 # Log File
-cp /var/log/install.log Logs/
+cp -p /var/log/install.log Logs/
 
 
 echo "----------------------------"
--- readme.md
+++ readme.md
@@ -3,16 +3,29 @@
 Author: Tobias Weisskopf
 Date: 2021-08-20
 
-
-Bash Script to get Forensic Relevant Artifacts from MacOS.
+Bash Script to get Forensic Relevant Artifacts from MacOS. Information on Forensic Artifacts used from 
+[1] (Brandt M., 2017)
 
 Getting System Information and Installed Applications
-
 
 [x] System Information
 [x] Installed Applications
 
 
-More features to be added.
+## Installation Instructions
 
+1. Clone Repository to your local machine
+2. Copy the repository to a USB Stick
+3. Connect USB Stick to the target device and 
+4. Open Terminal and change to the USB Stick
+5. Give Executable rights to the application `chmod +x get_system_info.sh`
+6. Execute the program with sudo `sudo ./get_system_info.sh`
 
+NOTE: By default the tool will copy the artifacts with `cp -p` to preserve timestamps. The tool will create respective folders in the same folder
+as the script location.
+
+---
+
+References
+
+[1] Brandt, M. (2017). Mac OS Hacking: Professionelle Werkzeuge und Methoden zur forensischen Analyse des Apple-Betriebssystems. Haar: Franzis Verlag.
